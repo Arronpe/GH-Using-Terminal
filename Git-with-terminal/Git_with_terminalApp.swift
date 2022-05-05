d2b831b49876a1ea6674014a4b5adda1b9723427
Arronpe <aeiesland75@hotmail.com>
2022-05-05 14:54:20 -0500

    Initial commit

---
+++ Git-with-terminal/Git_with_terminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_with_terminalApp.swift
+//  Git-with-terminal
+//
+//  Created by Arron Eiesland on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Git_with_terminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
